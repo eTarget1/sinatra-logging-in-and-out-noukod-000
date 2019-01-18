e36377ebe1427f83b8b184cc848033f781b4839d
clifford <clifford@noukod.com>
2019-01-18 21:31:34 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def self.current_user(session)
-  @user =  User.find(session[:user_id])
-  end
-
-  def self.is_logged_in?(session)
-    session.hash_key [:user_id]
-  end
+  # def self.current_user(session)
+  # @user =  User.find(session[:user_id])
+  # end
+  # 
+  # def self.is_logged_in?(session)
+  #   session.hash_key [:user_id]
+  # end
 end
