9d2c21d16579b7c4235045950c0a3f53417f32f8
clifford <clifford@noukod.com>
2019-01-18 21:27:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
+ @user = User.find_by(username: params[:username])    if @current_user
       erb :account
     else
       erb :error
